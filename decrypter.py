5306e66e7d7c7decfcea3a5edbaac3b9f1796bd7
RokuNine <94667767+RokuNine@users.noreply.github.com>
2022-03-07 20:45:52 -0800

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,5 +1,7 @@
+# this file decrypts the text file containing the passwords
 from cryptography.fernet import Fernet
 
+# getting the encrypted key
 with open('key.key', 'rb') as filekey:
 	key = filekey.read()
 
@@ -12,7 +14,6 @@ with open('./pwtext.txt', 'rb') as enc_file:
 # decrypting the file
 decrypted = fernet.decrypt(encrypted)
   
-# opening the file in write mode and
-# writing the decrypted data
+# opening the file in write mode and writing the decrypted data
 with open('./pwtext.txt', 'wb') as dec_file:
     dec_file.write(decrypted)
