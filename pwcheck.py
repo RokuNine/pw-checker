0fb685e981b4d0a43f0bb225ad1c49ca315bf260
RokuNine <94667767+RokuNine@users.noreply.github.com>
2022-03-07 22:07:26 -0800

    Update pwcheck.py

--- pwcheck.py
+++ pwcheck.py
@@ -1,9 +1,10 @@
 import requests
 import hashlib
 import sys
+import cryptography.exceptions
 from cryptography.fernet import Fernet
 
-#This function captures the api from a password checker website for use in later functions.
+# This function captures the api from a password checker website for use in later functions.
 def request_api_data(query_char):
   url = 'https://api.pwnedpasswords.com/range/' + query_char
   res = requests.get(url)
@@ -11,7 +12,7 @@ def request_api_data(query_char):
     raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
   return res
 
-#This function checks if passwords have been leaked and creates a counter for each time the password has been leaked.
+# This function checks if passwords have been leaked and creates a counter for each time the password has been leaked.
 def get_password_leaks_count(hashes, hash_to_check):
   #This tuple splits the SHA-1 hash and the counter apart for each password for a proper counter.
   hashes = (line.split(':') for line in hashes.text.splitlines())
@@ -20,7 +21,7 @@ def get_password_leaks_count(hashes, hash_to_check):
       return count
   return 0
 
-#This function converts the password strings into a SHA-1 hash from the hashlib package that the api is able to read.
+# This function converts the password strings into a SHA-1 hash from the hashlib package that the api is able to read.
 def pwned_api_check(password):
   sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
   first5_char, tail = sha1password[:5], sha1password[5:]
@@ -28,7 +29,7 @@ def pwned_api_check(password):
   return get_password_leaks_count(response, tail)
 
 '''
-#This function uses inputs from the terminal to check for password safety.
+# This function uses inputs from the terminal to check for password safety.
 def main(args):
   for password in args:
     count = pwned_api_check(password)
@@ -45,19 +46,23 @@ with open('key.key', 'rb') as filekey:
 
 fernet = Fernet(key)
 
-# opening the encrypted file
 with open('./pwtext.txt', 'rb') as enc_file:
     encrypted = enc_file.read()
   
-decrypted = fernet.decrypt(encrypted)
-  
-# opening the file in write mode and writing the decrypted data, this gets rid of the encryption
-# if this file needs to be run again the encrypter.py file must be ran first
-with open('./pwtext.txt', 'wb') as dec_file:
-    dec_file.write(decrypted)
+# This except statement catches exceptions from the cryptography module in case the file is ran again without the encrypter
+# This passes the decryption command error if the text file is not encrypted.
+try:
+  decrypted = fernet.decrypt(encrypted)
+# writes the file in decrypted mode
+  with open('./pwtext.txt', 'wb') as dec_file:
+      dec_file.write(decrypted)
+except cryptography.exceptions.InvalidKey:
+  pass
+except cryptography.fernet.InvalidToken:
+  pass
 
 
-#This function reads from the text file of passwords
+# This function reads from the text file of passwords
 def main():
     with open('./pwtext.txt', 'rb') as pw_file:
         lines = pw_file.readlines()
